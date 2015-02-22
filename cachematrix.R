97d8c4ccbdb9b1a4c0e261d8f7875f035f705c5c
yevgeniy-samyshkin <yevgeniy.samyshkin@gsk.com>
2015-02-22 15:58:29 +0000

    Cache Inverted Matrix
    Minor changes to the comments in the text

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,8 @@
 
 ## The function  makeCacheMatrix() creates a special vector (list) that contains a function to 
 ## set and get the values of the matrix 
-## set and get the inverted matrix 
+## set and get the inverted matrix
+## The syntax is based on the Coursera assignment 
 
 
 makeCacheMatrix <- function(x = matrix()) {      
@@ -34,7 +35,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## 4. run xx<-makeCacheMatrix(matr)
 ## 5. run get_inverse <-cacheSolve(xx) - this perfoems matrix inversion and generates no message
 ## 6. ruN the above line again (or any subsequent number of times) get_inverse <-cacheSolve(xx)
-## 7. this generates message 'getting cached data' which suggests that prior calculation 
+## 7. this generates message 'getting cached data' which suggests that prior calculation was retrieved from cache 
 
 ## Input to this function is an arbitrary invertible matrix 10x10  shown in the Annex 
 
@@ -51,8 +52,10 @@ cacheSolve <- function(x, ...) {
         m
 }
 
+## The syntax is based on the Coursera assignment 
 
-## data - an arbitrary invertible matrix 
+
+## data - an arbitrary invertible matrix I have created 
 
 seq1<-c(1, 1, 2, 1, 1, 1, 1, 1, 1, 1)
 seq2<-c(0, 1, 1, 1, 1, 1, 1, 1, 1, 1)
@@ -74,7 +77,8 @@ get_inverse <-cacheSolve(xx)
 
 ## Below is the test for the task computations 
 
-## Inverted matrix I have calculated outside R: 
+## Inverted matrix (based on matrix matr above) I have calculated outside R:
+ 
 ##[1]  -0.5	  0.5	 -0.25   0.25   0   1.5  -1.5   0.25  -0.5   8.75
 ##[2]  -1.5	  2.5	 -0.25  -0.75   0   1.5  -1.5   0.25  -0.5   8.75
 ##[3]   1.5	 -1.5	  0.25  -0.25   0  -1.5   1.5  -0.25   0.5  -8.75
@@ -85,6 +89,8 @@ get_inverse <-cacheSolve(xx)
 ##[8]   0.5	 -0.5	  0.75  -0.75   0  -0.5   0.5   0.25  -0.5  -5.25
 ##[9]  -1	  1	 -0.5	   0.5    0   1    -1     0.5	   0     6.5
 ##[10]  0	  0	  0	   0	    0   0     0     0      0     1
+
+
 
 ## R output - produced by the cacheSolve() funtion 
 
@@ -99,19 +105,3 @@ get_inverse <-cacheSolve(xx)
 ## [8,]  0.5 -0.5  0.75 -0.75    0 -0.5  0.5  0.25 -0.5  -5.25
 ## [9,] -1.0  1.0 -0.50  0.50    0  1.0 -1.0  0.50  0.0   6.50
 ## [10,]  0.0  0.0  0.00  0.00    0  0.0  0.0  0.00  0.0  1.00 
-
-
-## R output - inverted matrix by cacheSolve(xx)
-##      seq1 seq2  seq3  seq4 seq5 seq6 seq7  seq8 seq9 seq10
-## [1,] -0.5  0.5 -0.25  0.25    0  1.5 -1.5  0.25 -0.5  8.75
-## [2,] -1.5  2.5 -0.25 -0.75    0  1.5 -1.5  0.25 -0.5  8.75
-## [3,]  1.5 -1.5  0.25 -0.25    0 -1.5  1.5 -0.25  0.5  -8.75
-## [4,]  0.5 -0.5  1.75 -0.75   -1 -0.5  0.5  1.25 -2.5  -8.25
-## [5,] -1.0  1.0 -2.00  2.00    1  1.0 -2.0 -1.00  2.0  17.00
-## [6,]  0.5 -0.5  0.25 -0.25    0 -0.5  0.5 -0.25  0.5  -3.75
-## [7,]  0.5 -0.5 -0.25  0.25    0 -0.5  1.5 -0.75  0.5  -7.25
-## [8,]  0.5 -0.5  0.75 -0.75    0 -0.5  0.5  0.25 -0.5  -5.25
-## [9,] -1.0  1.0 -0.50  0.50    0  1.0 -1.0  0.50  0.0   6.50
-## [10,]  0.0  0.0  0.00  0.00    0  0.0  0.0  0.00  0.0  1.00 
-
-
